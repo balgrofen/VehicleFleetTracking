8f5abacca545e53e1fca0505910e03bcc822b803
balgrofen <gellencsabakalman1@gmail.com>
2025-12-21 22:06:34 +0100

    Added obsidian for better documentation

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+## Könyvtárak
+
+### TinyGPSPlus
+A TinyGPS++ objektum orientáltá teszi és értelmezhetőbbé teszi a GPS modulból érkező adatokat. Az alábbi linken elérhető a szerző github oldala. https://github.com/mikalhart/TinyGPSPlus
+	Jelenlegi verzió:1.0.3
+### TinyGSM 
+A TinyGSM a mobiladat kapcsolat létrejöttéhez szükséges. Illetve a modulból érkező gps adat kinyeréshez.
+https://github.com/vshymanskyy/TinyGSM
+	Jelenlegi verzió:0.12.0
+
+### ArduinoJson
+A modulokból érkező adatok JSON formában való feldolgozásához szükséges.
+	Jelenlegi verzió: 7.4.2
+
---
+++ firmware/GPSConnection/GPSConnection.ino
@@ -0,0 +1,76 @@
+#define TINY_GSM_MODEM_SIM7600
+#include <TinyGsmClient.h>
+
+const int pwr_pin = 4;
+#define MODEM_RX 26
+#define MODEM_TX 27
+
+HardwareSerial SerialAT(1);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // Power sequence
+  pinMode(pwr_pin, OUTPUT);
+  digitalWrite(pwr_pin, LOW);
+  delay(100);
+  digitalWrite(pwr_pin, HIGH);
+  delay(500);
+  digitalWrite(pwr_pin, LOW);
+
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+
+  Serial.println("Wait for 'OK' response from modem...");
+  
+  // Keep sending AT until the modem responds
+  bool modemReady = false;
+  while (!modemReady) {
+    SerialAT.println("AT");
+    delay(500);
+    while (SerialAT.available()) {
+      String res = SerialAT.readString();
+      if (res.indexOf("OK") != -1) {
+        modemReady = true;
+        Serial.println("Modem is responding!");
+      }
+    }
+    Serial.print("."); // Progress dots
+  }
+
+  // Now that we know it's awake, start GPS
+  Serial.println("\nStarting GPS Engine...");
+  SerialAT.println("AT+CGPS=1");
+  delay(500);
+  
+  Serial.println("\nResetting GPS settings...");
+  
+  // 1. Stop GPS first (in case it's already running)
+  SerialAT.println("AT+CGPS=0"); 
+  delay(1000);
+  
+  // 2. Set GPS to "Stand-alone" mode (Mode 1)
+  SerialAT.println("AT+CGPS=1,1"); 
+  delay(1000);
+
+  // 3. Try to query the location directly to see if it works
+  SerialAT.println("AT+CGPSINFO"); 
+  delay(500);
+
+  Serial.println("Enabling NMEA Stream...");
+  SerialAT.println("AT+CGPSOUT=255");
+  delay(500);
+}
+
+void loop() {
+  while (SerialAT.available()) {
+    String line = SerialAT.readStringUntil('\n');
+    Serial.println("MODEM: " + line);
+  }
+
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    SerialAT.println(cmd);
+    Serial.println("SENT: " + cmd);
+  }
+}

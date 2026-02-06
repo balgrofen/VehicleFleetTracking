70976cb531f54bf817e1431113282898558c27e0
balgrofen <gellencsabakalman1@gmail.com>
2026-02-06 12:54:22 +0100

    added mqtt parsing script

---
+++ backend/parsingMQTT.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import json
+import paho.mqtt.client as mqtt
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("./vehiclefleettracking-2e69e-firebase-adminsdk-fbsvc-82d5e1c903.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def save_to_firebase(carplate, data):
+    ts_ms = int(
+        datetime.fromisoformat(
+            data["timestamp"].replace("Z", "+00:00")
+        ).timestamp() * 1000
+    )
+
+    db.collection("vehicles") \
+      .document(carplate) \
+      .collection("locations") \
+      .document(str(ts_ms)) \
+      .set(data)
+
+def parse_payload(payload: str):
+    fields = payload.split(",")
+    carplate = str(fields[0])
+    lat = float(fields[1])
+    lon = float(fields[2])
+    speed = float(fields[3])
+    altitude = float(fields[4])
+    hdop = float(fields[7])
+
+    year, month, day = map(int, fields[8:11])
+    hour, minute, second = map(int, fields[11:14])
+
+    timestamp = datetime(
+        year, month, day, hour, minute, second
+    ).isoformat() + "Z"
+
+    return carplate, {
+        "lat": lat,
+        "lon": lon,
+        "speed": speed,
+        "altitude": altitude,
+        "accuracy": hdop,
+        "timestamp": timestamp
+    }
+
+
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode()
+    carplate,parsed = parse_payload(payload)
+    save_to_firebase(carplate, parsed)
+    print(parsed)
+
+client = mqtt.Client()
+client.on_message = on_message
+client.connect("localhost", 23295)
+client.subscribe("esp32/location")
+client.loop_forever()

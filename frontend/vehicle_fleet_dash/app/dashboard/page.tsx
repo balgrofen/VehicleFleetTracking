c14fabbcd309112d7e9e3c81c352d49c05490040
balgrofen <gellencsabakalman1@gmail.com>
2026-02-19 18:40:13 +0100

    magyar feliratok

--- frontend/vehicle_fleet_dash/app/dashboard/page.tsx
+++ frontend/vehicle_fleet_dash/app/dashboard/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
-                    Build Your Application
+                    Jármű követés
                   </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
